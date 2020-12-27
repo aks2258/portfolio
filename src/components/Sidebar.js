bd5437c64ad9572b1b95f452229296f4e447d813
aks2258 <arniekidd88@yahoo.com>
2020-12-27 16:07:46 -0600

    Added Sidebar.

---
+++ src/components/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import {
+    Icon,
+    Menu,
+    Sidebar,
+  } from 'semantic-ui-react'
+
+function SidebarNav(props) {
+
+    return(
+        <Sidebar
+            as={Menu}
+            animation='push'
+            icon='labeled'
+            inverted
+            vertical
+            visible={props.toggleMenu}
+            width='thin'
+          >
+            <Menu.Item as= 'a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='archive' />
+                    Projects
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='phone' />
+              Contact
+            </Menu.Item>
+          </Sidebar>
+    )
+}
+
+export default SidebarNav
