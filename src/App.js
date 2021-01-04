15b6f1dd0c17745641b26f2d538854975bd3a282
aks2258 <arniekidd88@yahoo.com>
2021-01-04 12:20:13 -0600

    Implemented carousel for projects, added necessary CS for carousel to display properly.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import './App.css';
 import NavBar from "./components/NavBar"
 import SidebarNav from "./components/Sidebar"
+import Projects from "./components/Projects"
+import cx from 'classnames'
+
 
 function App() {
   
@@ -11,11 +14,20 @@ function App() {
     setToggle(!toggle)
   }
 
+  const classes = cx(
+    'pusher',
+    'button',
+    {'dimmed': toggle}
+  )
+
   return (
     <div className="main-page">
     <NavBar onToggleMenu={ handleToggleMenu } />
     <div className='ui attached pushable' style ={{height: '100vh'}} >
       <SidebarNav toggleMenu = {toggle}/>
+      <div className={classes}>
+        <Projects />
+      </div>
     </div>
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import { Button } from 'semantic-ui-react'
+
+export default () => (
+  <Carousel autoPlay showThumbs = {false} infiniteLoop = {true}>
+    <div>
+        <img alt="" src="https://raw.githubusercontent.com/aks2258/Typing-Test/main/images/Home.png" />
+            <div className="legend">
+                    <Button inverted color='blue'>
+                        Typing Test
+                    </Button>
+            </div>
+    </div>
+    <div>
+        <img alt="" src="https://raw.githubusercontent.com/aks2258/Playbase/master/playbase_login.png" />
+            <div className="legend">
+                <Button inverted color='black'>
+                    Playbase
+                </Button>
+            </div>
+    </div>
+    <div>
+        <img alt="" src="https://raw.githubusercontent.com/aks2258/mtg_project_frontend/main/src/images/Home.png" />
+            <div className="legend">
+                <Button  inverted color='green'>
+                    Imperfect Gathering
+                </Button>
+            </div>
+    </div>
+  </Carousel>
+);
