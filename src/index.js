6c154a2eee0ecefce0b1ea2380f86bc461447363
aks2258 <arniekidd88@yahoo.com>
2021-01-25 22:34:11 -0600

    Minor updates.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import 'semantic-ui-css/semantic.min.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
