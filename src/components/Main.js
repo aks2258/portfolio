625fa74fdaf1091341aceaabd2d6b59730b7c445
aks2258 <arniekidd88@yahoo.com>
2021-02-08 17:21:19 -0600

    Added Grid Columns to center Welcome text.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,18 @@ import { Grid, Header } from 'semantic-ui-react'
 
 function Main () {
     return(
-        <Grid verticalAlign='middle' centered>
+        <Grid verticalAlign='middle' columns={5} centered>
         <Grid.Row>
           <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
           <Header size='huge' color='grey'>Welcome</Header>
+          </Grid.Column>
+          <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
           </Grid.Column>
         </Grid.Row>
       </Grid>
