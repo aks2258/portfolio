e14200c2bb504b83a3c4412af6b54fae50b5b6d4
aks2258 <arniekidd88@yahoo.com>
2021-02-08 17:37:48 -0600

    Removed Semantic grid layout in favor of pure CSS animation with Welcome text.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,21 +3,9 @@ import { Grid, Header } from 'semantic-ui-react'
 
 function Main () {
     return(
-        <Grid verticalAlign='middle' columns={5} centered>
-        <Grid.Row>
-          <Grid.Column>
-          </Grid.Column>
-          <Grid.Column>
-          </Grid.Column>
-          <Grid.Column>
-          <Header size='huge' color='grey'>Welcome</Header>
-          </Grid.Column>
-          <Grid.Column>
-          </Grid.Column>
-          <Grid.Column>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+        <main>
+            <span><p>Welcome</p></span>
+        </main>
     )
 }
 
