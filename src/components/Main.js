38bf57bb2bf9898b1b4167b2825207c137318dea
aks2258 <arniekidd88@yahoo.com>
2021-02-08 20:12:42 -0600

    added some placeholder text until CSS is fixed to be easier to read and more visually appealing. Reimplemented Grid layout that has Header components that will be replaced with Button component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,27 @@ import { Grid, Header } from 'semantic-ui-react'
 
 function Main () {
     return(
+        <div className="text-div">
         <main>
-            <span><p>Welcome</p></span>
+            <span><p className = "welcome-text">Welcome</p></span>
         </main>
+            <p>to my website. You can use the following buttons to navigate or use the sidebar
+        once you're on any page.</p>
+        <Grid verticalAlign='middle' columns={4} centered>
+            <Grid.Row>
+            <Grid.Column>
+                <Header color = "grey">About Me</Header>
+            </Grid.Column>
+            <Grid.Column>
+                <Header color = "grey">Projects</Header>
+                <br />
+            </Grid.Column>
+            <Grid.Column>
+                <Header color = "grey">Contact</Header>
+            </Grid.Column>
+            </Grid.Row>
+        </Grid>
+        </div>
     )
 }
 
